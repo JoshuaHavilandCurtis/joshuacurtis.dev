ce0cf5142768196cb73d7d79e9787f657b9ee31b
Josh <josh@192.168.1.113>
2021-08-20 20:17:50 +0100

    tailwind cdn and portfolio images

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Comfortaa:700" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.2/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(). '/css/style.css' ?>">
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(). '/css/cursor.css' ?>">
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(). '/css/skills.css' ?>">
--- template-parts/content/work.php
+++ template-parts/content/work.php
@@ -11,13 +11,13 @@
     </div>
     <h1>My work.</h1>
     <section>
-        <div class="flex justify-between">
+        <div class="flex">
             <div class="self-center">
                 <img class="portfolio-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/dianebutterworth-portfolio.jpg">
             </div>
             <div class="self-center">
                 <h2>Diane Butterworth</h2>
-                <h3>Designed and Built by <a class="link-work" href="https://chaptr.studio/" target="_blank">Chaptr</a></h3>
+                <h3>Designed and Built by <a class="link-work" href="https://chaptr.studio/" target="_blank">Chaptr</a></h3><br>
                 <p>Diane Butterworth Ltd is an office planning, design, and technical consultancy.</p>    
                 <p>From design to technical products to construction they can support your digital requirements.</p>
                 <p>Using the latest technology, We built Diane's website in WordPress (CMS) using Advanced Custom Fields, Sage Theme, Bedrock, PHP and Javascript.</p>
@@ -39,13 +39,13 @@
 
 
     <section>
-        <div class="flex justify-between">
-            <div class="self-end">
-                <img class="portfolio-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/dianebutterworth-portfolio.jpg">
+        <div class="flex">
+            <div class="self-center">
+                <img class="portfolio-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/2025-portfolio.jpg">
             </div>
             <div class="self-center">
                 <h2>Southampton 2025</h2>
-                <h3>Designed and Built by <a class="link-work" href="https://southampton2025.co.uk/" target="_blank">Chaptr</a></h3>
+                <h3>Designed and Built by <a class="link-work" href="https://southampton2025.co.uk/" target="_blank">Chaptr</a></h3><br>
                 <p>This is our Southampton</p>    
                 <p>Southampton is bidding to become the UK City of Culture 2025.</p>
                 <p>t’s time to shout about Southampton</p>
@@ -67,12 +67,12 @@
     </section>
 
     <section>
-        <div class="flex justify-between">
-            <div>
+        <div class="flex">
+            <div class="self-center">
                 <img class="portfolio-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/ballroofing-portfolio.jpg">
             </div>
-            <div>
-                <h2>Ball Roofing & Property</h2>   
+            <div class="self-center">
+                <h2>Ball Roofing & Property</h2><br>   
                 <p>Ball Roofing & Property Limited specialise in roofing<br> and building and are experts in high performance torch on felt systems.</p>
                 <p>They undertake all aspects of roofing. Warm and cold roof construction, new build, refurbishment and repair.</p>
             </div>
@@ -93,11 +93,11 @@
     <section>
         <div class="flex">
             <div class="self-center">
-                <img class="portfolio-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/dianebutterworth-portfolio.jpg">
+                <img class="portfolio-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/bridge2aid-portfolio.jpg">
             </div>
             <div class="self-center">
                 <h2>Bridge 2 Aid</h2>
-                <h3>Designed and Built by <a class="link-work" href="https://dental-design.marketing/" target="_blank">Dental Design Marketing</a></h3>
+                <h3>Designed and Built by <a class="link-work" href="https://dental-design.marketing/" target="_blank">Dental Design Marketing</a></h3><br>
                 <p>Bridge2Aid fights the causes of poverty in some of the poorest communities in the developing world.</p>    
                 <p>Bridge2Aid believes that everybody has the right to safe emergency dental care.</p>
             </div>
